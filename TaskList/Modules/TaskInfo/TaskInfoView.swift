32b889ee4a6479d6926c8cbe119cbd11d6922e91
TheDanz <ryumin_danil@icloud.com>
2025-08-05 02:32:21 +0300

    Implement TaskInfoView

--- TaskList/Modules/TaskInfo/TaskInfoView.swift
+++ TaskList/Modules/TaskInfo/TaskInfoView.swift
@@ -6,10 +6,40 @@ protocol TaskInfoView {
 
 final class TaskInfoViewImpl: UIViewController {
     
+    // MARK: - Views
+    
+    private lazy var titleTextView: UITextView = .style {
+        $0.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        $0.textColor = .mainText
+        $0.backgroundColor = .mainBackground
+        $0.isScrollEnabled = false
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private lazy var creationDateLabel: UILabel = .style {
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = .searchTextFieldElements
+        $0.numberOfLines = 1
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private lazy var descriptionTextView: UITextView = .style {
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        $0.textColor = .mainText
+        $0.backgroundColor = .mainBackground
+        $0.isScrollEnabled = false
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
     // MARK: - Inits
     
     init(task: TaskListEntity) {
         super.init(nibName: nil, bundle: nil)
+        titleTextView.text = task.title
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yy"
+        creationDateLabel.text = formatter.string(from: task.creationDate)
+        descriptionTextView.text = task.description
     }
     
     required init?(coder: NSCoder) {
@@ -20,5 +50,52 @@ final class TaskInfoViewImpl: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - UI
+    
+    private func setupUI() {
+        setupView()
+        setupNavigationController()
+        setupAutoLayout()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .mainBackground
+        view.addSubview(titleTextView)
+        view.addSubview(creationDateLabel)
+        view.addSubview(descriptionTextView)
+    }
+    
+    private func setupNavigationController() {
+        navigationController?.navigationBar.isHidden = false
+        navigationController?.navigationBar.tintColor = .golden
+    }
+    
+    // MARK: - AutoLayout
+    
+    private func setupAutoLayout() {
+        setupTitleTextViewConstraints()
+        setupCreationDateLabelConstraints()
+        setupDescriptionTextViewConstraints()
+    }
+    
+    private func setupTitleTextViewConstraints() {
+        titleTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        titleTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        titleTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+    }
+    
+    private func setupCreationDateLabelConstraints() {
+        creationDateLabel.topAnchor.constraint(equalTo: titleTextView.bottomAnchor, constant: 8).isActive = true
+        creationDateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
+        creationDateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+    }
+    
+    private func setupDescriptionTextViewConstraints() {
+        descriptionTextView.topAnchor.constraint(equalTo: creationDateLabel.bottomAnchor, constant: 16).isActive = true
+        descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
     }
 }
